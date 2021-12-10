426a275c3dfd36d66515bfd006174079321f0ad9
SProga <shane.306@hotmail.com>
2021-12-10 11:49:46 -0400

    final mailinglist

---
+++ js/modules/Features.js
@@ -0,0 +1,32 @@
+class Features {
+	constructor() {
+		this.overlay = document.querySelector(".overlay");
+		this.overlayShown = false;
+		this.card = document.querySelector(".card");
+		this.cardBtns = document.querySelectorAll(".card__form__preference");
+		this.registerEvents();
+		this.displayCard();
+	}
+	registerEvents() {
+		this.cardBtns.forEach((btn) => {
+			btn.addEventListener("click", () => {
+				btn.classList.toggle("card__form__preference-active");
+			});
+		});
+	}
+
+	displayCard() {
+		setTimeout(() => {
+			this.overlay.classList.add("overlay__active");
+			this.overlayShown = true;
+			setTimeout(() => {
+				if (this.overlayShown) {
+					this.overlay.style.transition = "none"; //eradicate the glitch when resizing
+				}
+				this.card.classList.add("card__active");
+			}, 600);
+		}, 2000);
+	}
+}
+
+export default Features;
---
+++ js/script.js
@@ -0,0 +1,4 @@
+import Features from "./modules/Features.js";
+// I don't normally use classes I recommend functional (functions) programming but for now I will use a class
+
+new Features();
